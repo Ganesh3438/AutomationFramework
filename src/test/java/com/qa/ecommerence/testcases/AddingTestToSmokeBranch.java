9e2b0bf1cf3b7b9cfd0c2b3ab64e9aca736de42d
= <ganesh2638@gmail.com>
2022-07-23 10:29:34 +0530

    Added test case into smoke branch[F

---
+++ src/test/java/com/qa/ecommerence/testcases/AddingTestToSmokeBranch.java
@@ -0,0 +1,12 @@
+package com.qa.ecommerence.testcases;
+
+public class AddingTestToSmokeBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Adding test case into smoke branch");
+
+	}
+
+}
--- src/test/java/selenium_activities/Demo.java
+++ src/test/java/selenium_activities/Demo.java
@@ -3,21 +3,23 @@ package selenium_activities;
 public class Demo {
 
 	public static void main(String[] args) {
-	
-		int temp = 0;
 		
-		for(int i=1; i<=100; i++) {
-			
-			for(int j=2; j<=i-1; j++) {
-				
-				if(i%j==0) {
+		System.out.println("Print prime numbers");
+
+		int temp = 0;
+
+		for (int i = 1; i <= 100; i++) {
+
+			for (int j = 2; j <= i - 1; j++) {
+
+				if (i % j == 0) {
 					temp = temp + 1;
 				}
-				
+
 			}
-			if(temp==0) {
+			if (temp == 0) {
 				System.out.println(i);
-			}else {
+			} else {
 				temp = 0;
 			}
 		}
