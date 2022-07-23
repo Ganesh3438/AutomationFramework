902ffb3f9b73120aeb4cb476bcbbab59d3394013
= <ganesh2638@gmail.com>
2022-07-23 21:15:48 +0530

    first commit into regression branch

---
+++ src/test/java/selenium_activities/RegressionFile.java
@@ -0,0 +1,11 @@
+package selenium_activities;
+
+public class RegressionFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Pushing the code into regression branch");
+	}
+
+}
