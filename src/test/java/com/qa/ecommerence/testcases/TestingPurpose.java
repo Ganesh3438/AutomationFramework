8c32ef0e3a1d3a271e3fea0a0ac6e84488ec7dea
= <ganesh2638@gmail.com>
2022-07-23 10:38:03 +0530

    added second test cases for smoke branch

---
+++ src/test/java/com/qa/ecommerence/testcases/TestingPurpose.java
@@ -0,0 +1,11 @@
+package com.qa.ecommerence.testcases;
+
+public class TestingPurpose {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing purpose");
+	}
+
+}
